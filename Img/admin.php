4bcf4ccc946caa372b1dcc215f526d94ba315c93
SquonK <sp@sqk.fr>
2021-05-18 20:52:51 +0200

    Img: UI for json editing

--- Img/admin.php
+++ Img/admin.php
@@ -58,28 +58,112 @@ if ($dossier = opendir($the_category['prefixe'])) {
 
 ?>
 <html>
-    <script type="text/javascript">
-        const existingFiles = <?php echo json_encode($tableau); ?>;
-        const knownFiles    = <?php echo json_encode($known_files) ?>;
-        const knownImages   = new Set(knownFiles.images.map(image => image.titre));
-        const missingFiles  = existingFiles.filter(file => !knownImages.has(file));
-        
-        const categories = ["titre", ...knownFiles.colonnes.map(c => c.balise)];
+    <head>
+        <style>
+            label {
+                width: 7em;
+                display: inline-block;
+            }
 
-        console.error(missingFiles);
-        console.error(categories);
+            input {
+                width: 20em;
+            }
 
-        // TODO: the UI
-        // TODO: dead links
-    </script>
+            td { text-align: center; }
+        </style>
+    </head>
     <body>
-        <table>
+        <table id="missing_table">
             <tr>
                 <th>Missing pictures</th>
             </tr>
         </table>
 
-        <textarea></textarea>
+        <textarea id="output" cols="80" rows="20"></textarea>
+
+        <script id="line" type="x-tmpl-mustache">
+                <td>
+                    <p><strong><a href="{{ link }}">{{ title }}</a></strong>
+                    {{#categories}}
+                        <br>
+                        <label for="{{image_id}}_{{id}}">{{name}}</label>
+                        <input type="text" name="{{image_id}}_{{id}}" id="input_{{image_id}}_{{id}}" oninput="refreshTextArea()" />
+                    {{/categories}}
+                    </p>
+                </td>                
+        </script>
+        <script src="https://unpkg.com/mustache@latest"></script>
+        <script type="text/javascript">
+            const existingFiles = <?php echo json_encode($tableau); ?>;
+            const knownFiles    = <?php echo json_encode($known_files) ?>;
+            const knownImages   = new Set(knownFiles.images.map(image => image.titre));
+            const missingFiles  = existingFiles.filter(file => !knownImages.has(file));
+            
+            const categories = ["titre", ...knownFiles.colonnes.map(c => c.balise)];
+            const enumeratedCategories = [...categories.entries()]
+                .map(t => { return { "id": t[0], "name": t[1] }; })
+                .filter(t => t.name !== 'date');
+
+            console.error(missingFiles);
+            console.error(categories);
+
+            document.addEventListener("DOMContentLoaded", function() {
+                const template = document.getElementById('line').innerHTML;
+                const table = document.querySelector('#missing_table');
+
+                for (const [i, missingFile] of missingFiles.entries()) {
+                    let render = Mustache.render(
+                        template,
+                        {
+                            title: missingFile,
+                            link: "<?php echo $the_category['prefixe']; ?>" + missingFile,
+                            image_id: i,
+                            categories: enumeratedCategories
+                        }
+                    );
+                    
+                    let tr = document.createElement("tr");
+                    tr.innerHTML = render;
+                    table.appendChild(tr);
+                }
+            });
+
+            function refreshTextArea() {
+                let content = [];
+
+                for (const [imageId, missingFile] of missingFiles.entries()) {
+                    let thisImage = {};
+
+                    let hasDate = false;
+
+                    for (const [categoryId, categoryName] of categories.entries()) {
+                        if (categoryName === 'date') {
+                            hasDate = true;
+                            continue;
+                        }
+
+                        const inputId = `input_${imageId}_${categoryId}`;
+                        const input = document.getElementById(inputId);
+                        if (input.value !== "") {
+                            thisImage[categoryName] = input.value;
+                        }
+                    }
+
+                    if (Object.keys(thisImage).length !== 0) {
+                        if (hasDate) {
+                            thisImage['date'] = new Date().toISOString().slice(0, 10);
+                        }
+
+                        content.push(thisImage);
+                    }
+                }
+
+                let output = document.getElementById("output");
+                output.value = JSON.stringify(content, null, 2);
+            }
+            
+            // TODO: dead links
+        </script>
     </body>
 </html>
 
